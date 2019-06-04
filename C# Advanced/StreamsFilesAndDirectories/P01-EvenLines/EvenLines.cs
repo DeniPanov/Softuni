06f43df8770c0f2073b35f318d5be790b7c01074
DeniPanov <Inedvonap@gmail.com>
2019-06-04 06:09:28 +0300

    Added P01-EvenLines and P02-LineNumbers.

---
+++ C# Advanced/StreamsFilesAndDirectories/P01-EvenLines/EvenLines.cs
@@ -0,0 +1,70 @@
+﻿namespace P01_EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        public static void Main()
+        {
+            using (var reader = new StreamReader("./text.txt"))
+            {
+                var line = reader.ReadLine();
+                var copyOfLine = new StringBuilder();
+
+                copyOfLine.Append(line);
+
+                int counter = 0;
+
+                while (line != null)
+                {
+                    if (counter % 2 == 0)
+                    {
+                        for (int i = 0; i < copyOfLine.Length; i++)
+                        {
+                            if (copyOfLine[i] == '-')
+                            {
+                                copyOfLine.Replace('-', '@');
+                            }
+                            else if (copyOfLine[i] == ',')
+                            {
+                                copyOfLine.Replace(',', '@');
+                            }
+                            else if (copyOfLine[i] == '.')
+                            {
+                                copyOfLine.Replace('.', '@');
+                            }
+                            else if (copyOfLine[i] == '!')
+                            {
+                                copyOfLine.Replace('!', '@');
+                            }
+                            else if (copyOfLine[i] == '?')
+                            {
+                                copyOfLine.Replace('?', '@');
+                            }
+                        }
+
+                        line = copyOfLine.ToString();
+
+                        var inputAsList = line
+                                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                .ToArray()
+                                .Reverse()
+                                .ToList();
+
+                        Console.WriteLine(string.Join(" ", inputAsList));
+
+                        inputAsList.Clear();
+                    }
+
+                    line = reader.ReadLine();
+                    copyOfLine.Clear();
+                    copyOfLine.Append(line);
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/StreamsFilesAndDirectories/P02-LineNumbers/LineNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace P02_LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        public static void Main()
+        {
+            string textPath = "text.txt";
+
+            var textLines = File.ReadAllLines(textPath);
+
+            int counter = 1;
+
+            string outputPath = "output-P02.txt";
+
+            foreach (var line  in textLines)
+            {
+                var lettersCount = line.Count(char.IsLetter);
+                var punctuationsCount = line.Count(char.IsPunctuation);
+
+                File.AppendAllText(outputPath, $"Line {counter}: {line} ({lettersCount})({punctuationsCount})" +
+                    $"{Environment.NewLine}");
+                counter++;
+            }
+        }
+    }
+}
