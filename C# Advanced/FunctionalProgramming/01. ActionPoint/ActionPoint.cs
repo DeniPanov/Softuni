e5b9e1feccab539711405359365e336d738099f5
DeniPanov <Inedvonap@gmail.com>
2019-06-09 07:33:47 +0300

    Added FunctionalProgrammig p01-p06

---
+++ C# Advanced/FunctionalProgramming/01. ActionPoint/ActionPoint.cs
@@ -0,0 +1,19 @@
+﻿namespace P01_ActionPoint
+{
+    using System;
+    using System.Linq;
+
+    public class ActionPoint
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                ToArray();
+
+            Action<string[]> printNames = n => Console.WriteLine(string.Join(Environment.NewLine, n));
+
+            printNames(input);
+        }
+    }
+}
