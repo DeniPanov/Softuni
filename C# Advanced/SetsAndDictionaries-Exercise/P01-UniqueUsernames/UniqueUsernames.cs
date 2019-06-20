c8b07e4fb2581278d0e94b1aa71f9c9dd9bc8615
DeniPanov <Inedvonap@gmail.com>
2019-06-20 20:23:46 +0300

    Added P01-UniqueUsernames and P02-SetsOfElements

---
+++ C# Advanced/SetsAndDictionaries-Exercise/P01-UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,35 @@
+﻿namespace P01_UniqueUsernames
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class UniqueUsernames
+    {
+        public static void Main()
+        {
+            int namesCount = int.Parse(Console.ReadLine());
+            var uniqueNamesHash = new HashSet<string>();
+
+            FillsHash(namesCount, uniqueNamesHash);
+
+            PrintsUniqueNames(uniqueNamesHash);
+        }
+
+        private static void PrintsUniqueNames(HashSet<string> uniqueNamesHash)
+        {
+            foreach (var name in uniqueNamesHash)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        private static void FillsHash(int namesCount, HashSet<string> uniqueNamesHash)
+        {
+            for (int i = 0; i < namesCount; i++)
+            {
+                string currentName = Console.ReadLine();
+                uniqueNamesHash.Add(currentName);
+            }
+        }
+    }
+}
---
+++ C# Advanced/SetsAndDictionaries-Exercise/P02-SetsOfElements/SetsOfElements.cs
@@ -0,0 +1,55 @@
+﻿namespace P02_SetsOfElements
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SetsOfElements
+    {
+        public static void Main()
+        {
+            var sizes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int nSetSize = int.Parse(sizes[0]);
+            int mSetSize = int.Parse(sizes[1]);
+
+            var nNumbersHash = new HashSet<int>(nSetSize);
+            var mNumbersHash = new HashSet<int>(mSetSize);
+            var hashToPrint = new HashSet<int>(nSetSize + mSetSize);
+
+            int nCounter = 0;
+
+            nCounter = FillsHashes(nSetSize, mSetSize, nNumbersHash, mNumbersHash, nCounter);
+
+            foreach (var element in nNumbersHash)
+            {
+                if (mNumbersHash.Contains(element))
+                {
+                    hashToPrint.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", hashToPrint));
+        }
+
+        private static int FillsHashes(int nSetSize, int mSetSize, HashSet<int> nNumbersHash, HashSet<int> mNumbersHash, int nCounter)
+        {
+            for (int i = 0; i < mSetSize + nSetSize; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (nCounter < nSetSize)
+                {
+                    nNumbersHash.Add(input);
+                }
+                else
+                {
+                    mNumbersHash.Add(input);
+                }
+
+                nCounter++;
+            }
+
+            return nCounter;
+        }
+    }
+}
