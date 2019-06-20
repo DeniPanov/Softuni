8495ad98419f803a3237e16ed03985f7251a2e48
DeniPanov <Inedvonap@gmail.com>
2019-06-20 06:41:52 +0300

    Added FightingArena

---
+++ C# Advanced/DefiningClasses/FightingArena/Arena.cs
@@ -0,0 +1,62 @@
+﻿namespace FightingArena
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Arena
+    {
+        private List<Gladiator> gladiators;
+
+        public Arena(string name)
+        {
+            gladiators = new List<Gladiator>();
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public int Count
+            => gladiators.Count();
+
+        public void Add(Gladiator gladiator)
+        {
+            gladiators.Add(gladiator);
+        }
+
+        public Gladiator Remove(string name) //Check the Arena; Consider void method;
+        {
+            Gladiator neededGladiator = gladiators.FirstOrDefault(n => n.Name == name);
+
+            if (neededGladiator != null)
+            {
+                gladiators.Remove(neededGladiator);
+            }
+
+            return neededGladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            Gladiator gladiator = gladiators.OrderByDescending(s => s.GetStatPower()).FirstOrDefault();
+            return gladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            Gladiator gladiator = gladiators.OrderByDescending(s => s.GetWeaponPower()).FirstOrDefault();
+            return gladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestTotalPower()
+        {
+            Gladiator gladiator = gladiators.OrderByDescending(s => s.GetTotalPower()).FirstOrDefault();
+            return gladiator;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {gladiators.Count} gladiators are participating.";
+        }
+    }
+}
---
+++ C# Advanced/DefiningClasses/FightingArena/Gladiator.cs
@@ -0,0 +1,49 @@
+﻿namespace FightingArena
+{
+    using System.Text;
+
+    public class Gladiator
+    {
+        public Gladiator(string name, Stat stat, Weapon weapon)
+        {
+            this.Name = name;
+            this.Stat = stat;
+            this.Weapon = weapon;
+        }
+
+        public string Name { get; set; }
+
+        public Stat Stat { get; set; }
+
+        public Weapon Weapon { get; set; }
+
+        public int GetTotalPower()
+        {
+            int totalPower = GetWeaponPower() + GetStatPower();
+            return totalPower;
+        }
+
+        public int GetWeaponPower()
+        {
+            int weaponPower = Weapon.Size + Weapon.Solidity + Weapon.Sharpness;
+            return weaponPower;
+        }
+
+        public int GetStatPower()
+        {
+            int statSum = Stat.Strength + Stat.Flexibility + Stat.Agility + Stat.Skills + Stat.Intelligence;
+            return statSum;
+        }
+
+        public override string ToString()
+        {
+            var textToPrint = new StringBuilder();
+
+            textToPrint.AppendLine("[Gladiator name] - [Gladiator total power]");
+            textToPrint.AppendLine("  Weapon Power: [Gladiator weapon power]");
+            textToPrint.AppendLine("  Stat Power: [Gladiator stat power]");
+
+            return textToPrint.ToString().TrimEnd();
+        }
+    }
+}
