59bde3e51f9846ac9240ec850fa977b75c3493a7
DeniPanov <Inedvonap@gmail.com>
2019-06-24 07:21:49 +0300

    Added P04-EvenTimes

---
+++ C# Advanced/SetsAndDictionaries-Exercise/P04-EvenTimes/EvenTimes.cs
@@ -0,0 +1,36 @@
+﻿namespace P04_EvenTimes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EvenTimes
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (dict.ContainsKey(input) == false)
+                {
+                    dict[input] = 1;
+                }
+                else
+                {
+                    dict[input]++;
+                }
+            }
+
+            foreach (var kvp in dict)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+            }
+        }
+    }
+}
