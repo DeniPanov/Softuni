de8857b36c89e6aed21e5639d776831d40f01a65
DeniPanov <Inedvonap@gmail.com>
2019-06-24 07:21:02 +0300

    Added P03-PeriodicTable

---
+++ C# Advanced/SetsAndDictionaries-Exercise/P03-PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,25 @@
+﻿namespace P03_PeriodicTable
+{
+    using System;
+    using System.Collections.Generic;
+    public class PeriodicTable
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var chemicalCompounds = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    chemicalCompounds.Add(input[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",chemicalCompounds));
+        }
+    }
+}
