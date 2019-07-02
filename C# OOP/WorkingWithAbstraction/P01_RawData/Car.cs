18cbbe2b2aeff818dc79790ebfd1dc0e3221edfd
DeniPanov <Inedvonap@gmail.com>
2019-07-02 06:46:26 +0300

    Added P01_RawData

---
+++ C# OOP/WorkingWithAbstraction/P01_RawData/Car.cs
@@ -0,0 +1,42 @@
+﻿namespace P01_RawData
+{
+    using System.Collections.Generic;
+    public class Car
+    {
+        private List<Tire> tires;
+
+        public string Model { get; set; }
+
+        public Tire Tire { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+        public List<Tire> Tires { get => tires; set => tires = value; }
+
+        public Car(string model, int engineSpeed, int enginePower,
+            int cargoWeight, string cargoType,
+            double tire1Pressure, int tire1Age,
+            double tire2Pressure, int tire2Age,
+            double tire3Pressure, int tire3Age,
+            double tire4Pressure, int tire4Age)
+        {
+            Tires = new List<Tire>(4);
+
+            Engine = new Engine(engineSpeed,enginePower);
+            Cargo = new Cargo(cargoWeight,cargoType);
+
+            this.Model = model;
+
+            Tire tire1 = new Tire(tire1Pressure, tire1Age);
+            Tire tire2 = new Tire(tire2Pressure, tire2Age);
+            Tire tire3 = new Tire(tire3Pressure, tire3Age);
+            Tire tire4 = new Tire(tire4Pressure, tire4Age);
+
+            Tires.Add(tire1);
+            Tires.Add(tire2);
+            Tires.Add(tire3);
+            Tires.Add(tire4);
+        }
+    }
+}
---
+++ C# OOP/WorkingWithAbstraction/P01_RawData/Startup.cs
@@ -0,0 +1,67 @@
+﻿namespace P01_RawData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            List<Car> cars = new List<Car>();
+
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] parameters = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string model = parameters[0];
+
+                int engineSpeed = int.Parse(parameters[1]);
+                int enginePower = int.Parse(parameters[2]);
+
+                int cargoWeight = int.Parse(parameters[3]);
+                string cargoType = parameters[4];
+
+                Tire tire1 = new Tire(double.Parse(parameters[5]), int.Parse(parameters[6]));
+                Tire tire2 = new Tire(double.Parse(parameters[7]), int.Parse(parameters[8]));
+                Tire tire3 = new Tire(double.Parse(parameters[9]), int.Parse(parameters[10]));
+                Tire tire4 = new Tire(double.Parse(parameters[11]), int.Parse(parameters[12]));
+
+                Car car = new Car(model,
+                    engineSpeed, enginePower,
+                    cargoWeight, cargoType,
+                    tire1.Pressure, tire1.Age,
+                    tire2.Pressure, tire2.Age,
+                    tire3.Pressure, tire3.Age,
+                    tire4.Pressure, tire4.Age);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                List<string> fragile = cars
+                    .Where(x => x.Cargo.Type == "fragile" && x.Tires.Any(y => y.Pressure < 1))
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, fragile));
+            }
+
+            else
+            {
+                List<string> flamable = cars
+                    .Where(x => x.Cargo.Type == "flamable" && x.Engine.Power > 250)
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, flamable));
+            }
+        }
+    }
+}
