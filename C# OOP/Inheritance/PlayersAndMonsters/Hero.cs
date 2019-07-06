0fe8a88813bdc1f4ae43e981a749280a4b68e0c0
DeniPanov <Inedvonap@gmail.com>
2019-07-06 10:36:56 +0300

    Added P03-PlayersAndMonsters

---
+++ C# OOP/Inheritance/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,36 @@
+﻿namespace PlayersAndMonsters
+{
+    using System;
+
+    public class Hero
+    {
+        private int level;
+
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        public string Username { get; set; }
+
+        public int Level
+        {
+            get => level;
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Hero level can not lesser than 1");
+                }
+
+                this.level = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
---
+++ C# OOP/Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                Elf wizard = new Elf("myWizard", -1);
+                System.Console.WriteLine(wizard);
+            }
+
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }            
+        }
+    }
+}
