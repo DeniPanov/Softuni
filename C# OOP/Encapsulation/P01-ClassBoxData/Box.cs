d860d47e373eeec77a37ae9e72a174af192a1f3e
DeniPanov <Inedvonap@gmail.com>
2019-07-10 07:47:26 +0300

    Added P01-ClassBoxData

---
+++ C# OOP/Encapsulation/P01-ClassBoxData/Box.cs
@@ -0,0 +1,79 @@
+﻿namespace P01_ClassBoxData
+{
+    using System;
+
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            this.Length = lenght;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => length;
+
+            private set
+            {
+                ValidatePropValue(value, nameof(this.Length));
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+
+            private set
+            {
+                ValidatePropValue(value, nameof(this.Width));
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+
+            private set
+            {
+                ValidatePropValue(value, nameof(this.Height));
+
+                this.height = value;
+            }
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            double surfaceArea = 2 * length * width + 2 * length * height + 2 * width * height;
+            return surfaceArea;
+        }
+
+        public double CalculateLateralSurface()
+        {
+            double lateralSurface = 2 * length * height + 2 * width * height;
+            return lateralSurface;
+        }
+
+        public double CalculateVolume()
+        {
+            double volume = length * width * height;
+            return volume;
+        }
+        private static void ValidatePropValue(double value, string parameterName)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{parameterName} cannot be zero or negative.");
+            }
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/P01-ClassBoxData/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace P01_ClassBoxData
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(lenght, width, height);
+
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurface():f2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+            }
+
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
