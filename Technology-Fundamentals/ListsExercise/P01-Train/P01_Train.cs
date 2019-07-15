9bea8ffbd384904cbc580e27f30bccc6784134fc
DeniPanov <Inedvonap@gmail.com>
2019-07-15 23:24:55 +0300

    Added P01_Train.cs

---
+++ Technology-Fundamentals/ListsExercise/P01-Train/P01_Train.cs
@@ -0,0 +1,53 @@
+﻿namespace P01_Train
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int maxPassengers = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+
+            while (true)
+            {
+                command = Console.ReadLine();
+                var text = new List<string>();
+                if (command == "end")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                    return;
+                }
+
+                text = command.Split().ToList();
+                int textAsNumber = 0;
+                string firstElementOfList = string.Empty;
+
+                for (int i = 0; i < text.Count; i++)
+                {
+                    if (text[0] == "Add")
+                    {
+                        textAsNumber = int.Parse(text[1]);
+                        numbers.Add(textAsNumber);
+                        break;
+                    }
+                    else if (text[0] != "Add")
+                    {
+                        textAsNumber = int.Parse(text[0]);
+                        for (int j = 0; j < numbers.Count; j++)
+                        {
+                            if (textAsNumber + numbers[j] <= maxPassengers)
+                            {
+                                numbers[j] += textAsNumber;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
