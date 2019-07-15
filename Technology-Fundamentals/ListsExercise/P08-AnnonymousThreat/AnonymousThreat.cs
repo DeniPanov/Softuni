ef8168336acd44350e254e85e762ccab8ee34ad0
DeniPanov <Inedvonap@gmail.com>
2019-07-15 23:28:02 +0300

    Added P08-AnnonymousThreat/AnonymousThreat.cs

---
+++ Technology-Fundamentals/ListsExercise/P08-AnnonymousThreat/AnonymousThreat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Anonymous_Threat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintAnonymousThreat();
+        }
+
+        private static void PrintAnonymousThreat()
+        {
+            var threat = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            while (true)
+            {
+                string comand = Console.ReadLine();
+                if (comand == "3: 1")
+                {
+                    Console.WriteLine(string.Join(" ", threat));
+                    return;
+                }
+                var comandSplit = comand.Split().ToList();
+                if (comandSplit[0] == "merge")
+                {
+                    int start = int.Parse(comandSplit[1]);
+                    int final = int.Parse(comandSplit[2]);
+                    while (start < final)
+                    {
+                        threat[int.Parse(comandSplit[1])] += threat[start + 1];
+                        start++;
+                    }
+                    start = int.Parse(comandSplit[1]);
+
+                    for (int i = final; i >= start + 1; i--)
+                    {
+                        threat.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+}
