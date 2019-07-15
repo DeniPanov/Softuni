6e7adce68f4573f4e1d52a7b233c90aae3f403af
DeniPanov <Inedvonap@gmail.com>
2019-07-15 23:25:31 +0300

    Added P02-ChangeList.cs

---
+++ Technology-Fundamentals/ListsExercise/P02-ChangeList/P02-ChangeList.cs
@@ -0,0 +1,46 @@
+﻿namespace P02_ChangeList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            CommandsManipulatingList();
+        }
+        public static void CommandsManipulatingList()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command = string.Empty;
+
+            while (true)
+            {
+                command = Console.ReadLine();
+                if (command == "end")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                    return;
+                }
+
+                var text = command.Split().ToList();
+                int textAsNumber = 0;
+                int lastElement = 0;
+
+                if (text[0] == "Delete")
+                {
+                    textAsNumber = int.Parse(text[1]);
+                    numbers.RemoveAll(x => x == textAsNumber);
+                }
+                else
+                {
+                    textAsNumber = int.Parse(text[1]);
+                    lastElement = int.Parse(text[2]);
+                    numbers.Insert(lastElement, textAsNumber);
+                }
+            }
+
+        }
+    }
+}
