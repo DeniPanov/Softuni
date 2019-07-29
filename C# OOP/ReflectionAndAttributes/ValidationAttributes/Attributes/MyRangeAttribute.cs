9eb110cd905557b07aa8802547cd251e10a906a8
DeniPanov <Inedvonap@gmail.com>
2019-07-29 07:24:31 +0300

    Added P02-ValidationAttributes

---
+++ C# OOP/ReflectionAndAttributes/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace ValidationAttributes.Attributes
+{
+    using System;
+
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            int objAsInt = Convert.ToInt32(obj);
+
+            if (objAsInt < this.minValue || objAsInt > this.maxValue)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes/ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Reflection;
+
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utilities
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] propertyInfos = obj
+                .GetType()
+                .GetProperties();
+
+            foreach (var property in propertyInfos)
+            {
+                MyValidationAttribute[] attributes = property
+                    .GetCustomAttributes()
+                    .Where(a => a is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                foreach (var attribute in attributes)
+                {
+                    if (attribute.IsValid(property.GetValue(obj)) == false)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
