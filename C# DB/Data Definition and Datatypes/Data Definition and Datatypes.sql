adb90c0f03e0ed590ae2489bd715106ee9f3c7ea
DeniPanov <Inedvonap@gmail.com>
2019-09-18 07:36:06 +0300

    Added p07

--- C# DB/Data Definition and Datatypes/Data Definition and Datatypes.sql
+++ C# DB/Data Definition and Datatypes/Data Definition and Datatypes.sql
@@ -47,3 +47,25 @@ TRUNCATE TABLE Minions
 
 DROP DATABASE Minions
 
+--Problem 7.	Create Table People
+
+CREATE TABLE People(
+Id INT PRIMARY KEY IDENTITY
+,Name NVARCHAR (200) NOT NULL
+,Picture VARBINARY (2048) 
+,Height DECIMAL (5 ,2)
+,Weight DECIMAL (5 ,2)
+,GENDER CHAR (1) NOT NULL CHECK (GENDER = 'm' OR GENDER = 'f')
+,Birthdate DATE NOT NULL
+,Biography NVARCHAR(MAX) 
+)
+
+INSERT INTO People(Name, Picture, Height, Weight, GENDER, Birthdate, Biography)VALUES
+('Deni', 200, 182, 94.8, 'm', '1990-05-15','Some random text...')
+,('Sanya', 500, 163, 53, 'f', '1991-05-19','Some random text...')
+,('Venci', 248, 178, 97, 'm', '1990-02-08','Some random text...')
+,('Gosho', 512, 185, 97, 'm', '1993-02-01','Some random text...')
+,('Pesho', 1024, 175, 80, 'm','1997-11-12','Some random text...');
+
+--Problem 8.	Create Table Users
+
