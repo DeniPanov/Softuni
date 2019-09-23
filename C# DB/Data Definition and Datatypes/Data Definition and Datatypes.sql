d66a1d52d2e049772ca2bb044f634ba39ebe1fce
DeniPanov <Inedvonap@gmail.com>
2019-09-23 23:44:16 +0300

    Added some more exercises...

--- C# DB/Data Definition and Datatypes/Data Definition and Datatypes.sql
+++ C# DB/Data Definition and Datatypes/Data Definition and Datatypes.sql
@@ -287,3 +287,63 @@ INSERT INTO RentalOrders ( EmployeeId, CustomerId, CarId, TankLevel, Kilometrage
 
 
 
+
+
+ --Problem 19.	Basic Select All Fields
+SELECT * 
+FROM Towns
+
+SELECT * 
+FROM Departments
+
+SELECT * 
+FROM Employees
+
+--Problem 20.	Basic Select All Fields and Order Them
+
+SELECT * 
+FROM Towns 
+ORDER BY Name ASC;
+
+SELECT *
+FROM Departments
+ORDER BY Name
+
+SELECT *
+FROM Employees
+ORDER BY Salary DESC;
+
+--Problem 21.	Basic Select Some Fields
+
+SELECT Name
+FROM Towns 
+ORDER BY Name
+
+SELECT Name
+FROM Departments
+ORDER BY Name 
+
+SELECT FirstName, LastName, JobTitle, Salary
+FROM Employees
+ORDER BY Salary DESC
+
+--Problem 22.	Increase Employees Salary
+
+UPDATE Employees
+SET Salary = Salary*1.1;
+
+SELECT Salary
+FROM Employees
+
+--Problem 23.	Decrease Tax Rate
+
+UPDATE Payments
+SET TaxRate -= TaxRate*0.03
+
+SELECT TaxRate 
+FROM Payments
+
+--Problem 24.	Delete All Records
+
+
+DELETE Occupancies
