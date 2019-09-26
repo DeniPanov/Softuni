38da169790ab5addc3e8661380f922373f194dbf
DeniPanov <Inedvonap@gmail.com>
2019-09-26 21:04:33 +0300

    Added the rest of the problems

--- C# DB/Basic CRUD/BasicCRUD.sql
+++ C# DB/Basic CRUD/BasicCRUD.sql
@@ -122,3 +122,24 @@ FROM Peaks
 ORDER BY PeakName
 
 --23. Biggest Countries by Population 
+
+SELECT TOP (30) CountryName, Population
+FROM Countries
+WHERE ContinentCode LIKE 'EU'
+ORDER BY Population DESC, CountryName
+
+--24. Countries and Currency (Euro / Not Euro) 
+
+SELECT CountryName, CountryCode,
+CASE
+	WHEN CurrencyCode = 'EUR' THEN 'Euro'
+	ELSE 'Not Euro'
+END AS Currency
+FROM Countries
+ORDER BY CountryName
+
+--25. All Diablo Characters 
+
+SELECT Name
+FROM Characters
+ORDER BY Name
