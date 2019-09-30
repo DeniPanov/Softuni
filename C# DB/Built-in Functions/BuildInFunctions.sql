a0aa924b3bac9022a8ae7089e499fb61c0d4c827
DeniPanov <Inedvonap@gmail.com>
2019-09-30 07:45:41 +0300

    Added p12 and p13

--- C# DB/Built-in Functions/BuildInFunctions.sql
+++ C# DB/Built-in Functions/BuildInFunctions.sql
@@ -80,3 +80,11 @@ SELECT CountryName, IsoCode
 FROM Countries
 WHERE CountryName LIKE '%a%a%a%'
 ORDER BY IsoCode
+
+--Problem 13. Mix of Peak and River Names
+
+SELECT PeakName, RiverName, 
+LOWER(PeakName + SUBSTRING(RiverName, 2, LEN(RiverName))) as Mix
+FROM Peaks, Rivers
+WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY Mix
