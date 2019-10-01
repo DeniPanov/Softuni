1766277e0dc83b020d368dc6decf3877c2765aa1
DeniPanov <Inedvonap@gmail.com>
2019-10-01 07:28:33 +0300

    Added all problems up to 17th (inclusive)

--- C# DB/Built-in Functions/BuildInFunctions.sql
+++ C# DB/Built-in Functions/BuildInFunctions.sql
@@ -88,3 +88,42 @@ LOWER(PeakName + SUBSTRING(RiverName, 2, LEN(RiverName))) as Mix
 FROM Peaks, Rivers
 WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
 ORDER BY Mix
+
+--Problem 14.	Games from 2011 and 2012 year
+
+SELECT TOP (50) Name, FORMAT(Start,'yyyy-MM-dd') AS Start
+ FROM Games
+ WHERE YEAR(Start) BETWEEN 2011 AND 2012
+ORDER BY Start, Name
+
+--Problem 15.	 User Email Providers
+
+SELECT Username, SUBSTRING(Email,CHARINDEX('@', Email) + 1,LEN(Email)) AS [Email Provider]
+FROM Users
+ORDER BY [Email Provider], Username
+
+--Problem 16.	 Get Users with IPAdress Like Pattern
+
+SELECT Username, IpAddress
+FROM Users
+WHERE IpAddress LIKE '___.1_%._%.___'
+ORDER BY Username
+
+--17. Show All Games with Duration 
+
+SELECT Name,
+CASE
+	WHEN DATEPART(HOUR,Start) BETWEEN 0 AND 11 THEN 'Morning' 
+	WHEN DATEPART(HOUR,Start) BETWEEN 12 AND 17 THEN 'Afternoon' 
+	WHEN DATEPART(HOUR,Start) BETWEEN 18 AND 23 THEN 'Evening'
+END AS [Part of the day],
+
+CASE 
+	WHEN Duration <= 3 THEN 'Extra Short '
+	WHEN Duration BETWEEN 4 AND 6 THEN 'Short '
+	WHEN Duration > 6 THEN 'Long'
+	WHEN Duration IS NULL THEN 'Extra Long'
+END AS Duration
+
+FROM Games
+ORDER BY Name, Duration, [Part of the day]
