0d56ea381b0f900154b39c91d0d6539cf46e61a5
DeniPanov <Inedvonap@gmail.com>
2019-10-01 18:48:03 +0300

    Added p09

--- C# DB/DataAgregation/DataAgregation.sql
+++ C# DB/DataAgregation/DataAgregation.sql
@@ -51,23 +51,23 @@ FROM WizzardDeposits
 GROUP BY DepositGroup, MagicWandCreator
 ORDER BY MagicWandCreator, DepositGroup 
 
---09. Age Groups / NOT FINISHED
+--09. Age Groups
 
-SELECT AgeGroup, COUNT(AgeGroup) AS WizardCount
-FROM
-(
-SELECT 
-CASE
-	WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
-	WHEN Age BETWEEN 11 AND 20 THEN '[11-10]'
-	WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
-	WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
-	WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
-	WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
-	WHEN Age > 60 THEN '[61+]'
-END AS AgeGroup
-FROM WizzardDeposits ) WC 
+SELECT wc.AgeGroup, COUNT(wc.AgeGroup) AS WizardCount
+FROM (
+		SELECT 
+			CASE
+			WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+			WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+			WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+			WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+			WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+			WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+			WHEN Age > 60 THEN '[61+]'
+		END AS AgeGroup
+		FROM WizzardDeposits )AS wc 
 GROUP BY AgeGroup
+
 
 --10. First Letter 
 
