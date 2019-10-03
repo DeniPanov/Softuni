c2c7a65f1c1ef26d439b194215b5a570e75bec5e
DeniPanov <Inedvonap@gmail.com>
2019-10-03 07:30:36 +0300

    Added p02 One-To-Many Relationship

--- C# DB/TableRelations/TablesRelations.sql
+++ C# DB/TableRelations/TablesRelations.sql
@@ -1,4 +1,4 @@
---Problem 1.	One-To-One Relationship
+--Problem 1. One-To-One Relationship
 
 CREATE TABLE Passports(
 PassportID INT PRIMARY KEY IDENTITY(101,1)
@@ -25,5 +25,33 @@ INSERT INTO Persons(FirstName, Salary,PassportID) VALUES
 ,('Tom', 56100.00, 103)
 ,('Yana', 60200.00,	101)
 
---Problem 2.	One-To-Many Relationship
+--Problem 2. One-To-Many Relationship
 
+CREATE TABLE Manufacturers(
+ManufacturerID INT PRIMARY KEY IDENTITY
+,Name NVARCHAR (30) NOT NULL
+,EstablishedOn DATE NOT NULL
+)
+
+CREATE TABLE Models(
+ModelID INT PRIMARY KEY IDENTITY(101,1)
+,Name NVARCHAR (30) NOT NULL
+,ManufacturerID INT NOT NULL
+CONSTRAINT FK_Models_Manufacturers
+FOREIGN KEY (ManufacturerID) REFERENCES Manufacturers(ManufacturerID)
+)
+
+INSERT INTO Manufacturers(Name, EstablishedOn) VALUES
+ ('BMW', '07/03/1916')
+,('Tesla', '01/01/2003')
+,('Lada', '01/05/1966')
+
+INSERT INTO Models(Name, ManufacturerID) VALUES
+('X1',	1)
+,('i6',	1)
+,('Model S', 2)
+,('Model X', 2)
+,('Model 3', 2)
+,('Nova', 3)
+		
+--Problem 3. Many-To-Many Relationship
