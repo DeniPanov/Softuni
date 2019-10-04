a49ff30c5e2ab87abf4c4d4bc92de78874aa63ae
DeniPanov <Inedvonap@gmail.com>
2019-10-04 06:59:30 +0300

    Added p04 Self-Referencing

--- C# DB/TableRelations/TablesRelations.sql
+++ C# DB/TableRelations/TablesRelations.sql
@@ -89,3 +89,21 @@ INSERT INTO StudentsExams(StudentID, ExamID) VALUES
 ,(3,	103)
 ,(2,	102)
 ,(2,	103)
+
+--Problem 4.	Self-Referencing 
+
+CREATE TABLE Teachers(
+TeacherID INT PRIMARY KEY IDENTITY(101,1)
+,Name NVARCHAR(30) NOT NULL
+,ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID)
+)
+
+INSERT INTO Teachers (Name, ManagerID) VALUES
+ ('John',	NULL)
+,('Maya',	106)
+,('Silvia',	106)
+,('Ted',	105)
+,('Mark',	101)
+,('Greta',	101)
+
+--Problem 5.	Online Store Database
