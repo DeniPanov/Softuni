cce4d070274244ff0ca2fc06d2ed45d38d555b89
DeniPanov <Inedvonap@gmail.com>
2019-10-04 07:31:49 +0300

    Added p05 Online Store Database

--- C# DB/TableRelations/TablesRelations.sql
+++ C# DB/TableRelations/TablesRelations.sql
@@ -90,7 +90,7 @@ INSERT INTO StudentsExams(StudentID, ExamID) VALUES
 ,(2,	102)
 ,(2,	103)
 
---Problem 4.	Self-Referencing 
+--Problem 4. Self-Referencing 
 
 CREATE TABLE Teachers(
 TeacherID INT PRIMARY KEY IDENTITY(101,1)
@@ -106,4 +106,38 @@ INSERT INTO Teachers (Name, ManagerID) VALUES
 ,('Mark',	101)
 ,('Greta',	101)
 
---Problem 5.	Online Store Database
+--Problem 5. Online Store Database
+
+CREATE TABLE Cities(
+CityID INT PRIMARY KEY
+,Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Customers(
+CustomerID INT PRIMARY KEY
+,Name VARCHAR(50) NOT NULL
+,Birthday DATE
+,CityID INT FOREIGN KEY REFERENCES  Cities(CityID) NOT NULL
+)
+
+CREATE TABLE Orders(
+OrderID INT PRIMARY KEY
+,CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID) NOT NULL
+)
+
+CREATE TABLE ItemTypes(
+ItemTypeID INT PRIMARY KEY
+,Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Items(
+ItemID INT PRIMARY KEY
+,Name VARCHAR(50) NOT NULL
+,ItemTypeID INT FOREIGN KEY REFERENCES ItemTypes(ItemTypeID) NOT NULL
+)
+
+CREATE TABLE OrderItems(
+OrderID INT FOREIGN KEY REFERENCES Orders(OrderID) NOT NULL
+,ItemID INT FOREIGN KEY REFERENCES Items(ItemID) NOT NULL
+CONSTRAINT PK_Composite_OrderID_ItemID PRIMARY KEY (OrderID, ItemID)
+)
