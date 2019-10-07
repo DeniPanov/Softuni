d9d5d20a951914d1c2f90d1ae5765363ad8deead
DeniPanov <Inedvonap@gmail.com>
2019-10-07 06:21:00 +0300

    Added p02 and p03

--- C# DB/SubqueriresAndJoins/SubqueriesAndJoins.sql
+++ C# DB/SubqueriresAndJoins/SubqueriesAndJoins.sql
@@ -7,3 +7,22 @@ ON a.AddressID = e.AddressID
 ORDER BY e.AddressID
 
 --Problem 2. Addresses with Towns
+
+SELECT TOP (50) e.FirstName, e.LastName, t.Name, a.AddressText
+FROM Employees as e
+JOIN Addresses as a
+ON a.AddressID = e.AddressID
+JOIN Towns as t
+ON t.TownID = a.TownID
+ORDER BY e.FirstName, e.LastName
+
+--Problem 3. Sales Employee
+
+SELECT e.EmployeeID, e.FirstName, e.LastName, d.Name
+FROM Employees as e
+JOIN Departments as d
+ON d.DepartmentID = e.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY e.EmployeeID
+
+
