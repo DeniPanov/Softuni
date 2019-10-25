3390bbd0440f798d2ea676b2b0b94eb0d58e9db1
DeniPanov <Inedvonap@gmail.com>
2019-10-25 07:10:40 +0300

    Added the solution file, StartUp.cs and Configuration.cs

---
+++ C# DB/Entity Framework Core/DB Apps Intro/Exercise/InitialSetup/Configuration.cs
@@ -0,0 +1,15 @@
+﻿namespace InitialSetup
+{
+    public class Configuration
+    {
+        public static string InitialConnectionString =
+            @"Server=DESKTOP-2A8QSKB\SQLEXPRESS;
+            ;
+            Integrated Security=true";
+
+        public static string ConnectionString =
+            @"Server=DESKTOP-2A8QSKB\SQLEXPRESS;
+            Database=MinionsDB;
+            Integrated Security=true";
+    }
+}
