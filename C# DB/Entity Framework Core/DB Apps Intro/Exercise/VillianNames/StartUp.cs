fad1ae5a319a781f5442250f048f4a9947d173f0
DeniPanov <Inedvonap@gmail.com>
2019-10-25 07:39:23 +0300

    Added p02 - VillianNames

---
+++ C# DB/Entity Framework Core/DB Apps Intro/Exercise/VillianNames/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace VillianNames
+{
+    using System;
+    using InitialSetup;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            GetAllVillainNamesAndTheCountOfTheirMinions();
+        }
+
+        private static void GetAllVillainNamesAndTheCountOfTheirMinions()
+        {
+            using (var connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string queryText = @"  SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                        FROM Villains AS v 
+                                        JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                    GROUP BY v.Id, v.Name 
+                                      HAVING COUNT(mv.VillainId) > 3 
+                                    ORDER BY COUNT(mv.VillainId)";
+
+                using (var command = new SqlCommand(queryText, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
