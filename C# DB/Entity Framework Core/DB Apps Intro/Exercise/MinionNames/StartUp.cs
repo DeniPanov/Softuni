cbb627c0564fa9b74ec1444f1131efdfd77a7243
DeniPanov <Inedvonap@gmail.com>
2019-10-29 07:08:31 +0200

    Added the rest of the problems

---
+++ C# DB/Entity Framework Core/DB Apps Intro/Exercise/MinionNames/StartUp.cs
@@ -0,0 +1,69 @@
+﻿namespace MinionNames
+{
+    using System;
+    using InitialSetup;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            static void Main(string[] args)
+            {
+                int id = int.Parse(Console.ReadLine());
+
+                using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+                {
+                    connection.Open();
+
+                    string villainsQuery = @"SELECT Name FROM Villains WHERE Id = @Id";
+
+                    using (SqlCommand command = new SqlCommand(villainsQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+                        string name = (string)command.ExecuteScalar();
+
+                        if (name == null)
+                        {
+                            Console.WriteLine($"No villain with ID {id} exists in the database.");
+                            return;
+                        }
+
+                        Console.WriteLine($"Villain: {name}");
+                    }
+
+                    string minionsQuery = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = @Id
+                                ORDER BY m.Name";
+
+                    using (SqlCommand command = new SqlCommand(minionsQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                long row = (long)reader[0];
+                                string name = (string)reader[1];
+                                int age = (int)reader[2];
+
+                                Console.WriteLine($"{row}. {name} {age}");
+                            }
+
+                            if (!reader.HasRows)
+                            {
+                                Console.WriteLine("(no minions)");
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/DB Apps Intro/Exercise/PrintAllMinionNames/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace PrintAllMinionNames
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.Collections.Generic;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                List<string> printName = new List<string>();
+
+                string minionQuery = @"SELECT Name FROM Minions";
+
+                using (SqlCommand command = new SqlCommand(minionQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            printName.Add((string)reader[0]);
+                        }
+
+                        for (int i = 0; i < (printName.Count - 1) / 2; i++)
+                        {
+                            Console.WriteLine(printName[i]);
+                            Console.WriteLine(printName[printName.Count - 1 - i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
