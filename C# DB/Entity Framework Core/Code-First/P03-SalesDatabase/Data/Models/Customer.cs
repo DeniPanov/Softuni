373d1b625dc6781b4fccc4d361ece25f8f15287b
DeniPanov <Inedvonap@gmail.com>
2019-11-08 22:00:20 +0200

    Updated the solution file; Added p03, p04 and p05 with all entities needed

---
+++ C# DB/Entity Framework Core/Code-First/P03-SalesDatabase/Data/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿namespace P03_SalesDatabase.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(80)]
+        [Required]
+        public string Email { get; set; }
+
+
+        [Required]
+        public string CreditCardNumber { get; set; }
+
+        public int SalesId { get; set; }
+
+        public ICollection<Sale> Sales { get; set; } = new HashSet<Sale>();
+
+    }
+}
---
+++ C# DB/Entity Framework Core/Code-First/P03-SalesDatabase/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace P03_SalesDatabase
+{
+    using P03_SalesDatabase.Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SalesDatabaseContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+}
