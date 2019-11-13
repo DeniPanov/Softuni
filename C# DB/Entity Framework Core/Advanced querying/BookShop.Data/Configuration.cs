52ab3c0fde7785e98641894b3c0eb53fcda8e895
DeniPanov <Inedvonap@gmail.com>
2019-11-13 07:32:34 +0200

    Added p00 - Book Shop Database + all conditions

---
+++ C# DB/Entity Framework Core/Advanced querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString => @"Server=DESKTOP-2A8QSKB\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ C# DB/Entity Framework Core/Advanced querying/BookShop/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(db);
+
+                string input = Console.ReadLine();
+
+                string result = GetBooksByAgeRestriction(db, input);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var books = context
+                .Books
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
