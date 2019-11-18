0b2dff9a3b6fbe6342a9dc5fd479c94118f63863
DeniPanov <Inedvonap@gmail.com>
2019-11-18 08:01:33 +0200

    Added p01 - Import Users + all entities needed

---
+++ C# DB/Entity Framework Core/Josn Processing/ProductShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace ProductShop
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    using ProductShop.Data;
+    using ProductShop.Models;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new ProductShopContext())
+            {
+                string input = File.ReadAllText(@"./../../../Datasets/users.json");
+
+                string result = ImportUsers(db, input);
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+    }
+}
