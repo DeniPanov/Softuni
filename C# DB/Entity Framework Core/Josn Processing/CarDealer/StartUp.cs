5cc99f61a238ada742288d4e10c35014bf3dcdda
DeniPanov <Inedvonap@gmail.com>
2019-11-22 21:07:28 +0200

    Added all needed files + p09 - ImportSuppliers

---
+++ C# DB/Entity Framework Core/Josn Processing/CarDealer/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace CarDealer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using AutoMapper;
+    using CarDealer.Data;
+    using CarDealer.Models;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var db = new CarDealerContext())
+            {
+                string path = @"./../../../Datasets/suppliers.json";
+                string inputJson = File.ReadAllText(path);
+
+                string result = ImportSuppliers(db, inputJson);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        //p09 - Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}.";//String.Format(outputMessage,suppliers.Count());
+        }
+    }
+
+}
