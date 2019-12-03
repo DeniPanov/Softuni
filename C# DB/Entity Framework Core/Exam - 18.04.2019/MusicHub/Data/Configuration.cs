102bc917fdb0e888bcfedff1585b97986392b834
DeniPanov <Inedvonap@gmail.com>
2019-12-03 06:46:42 +0200

    Modified Configuration.cs so it runs on my local server; Added all DbSets and table relations;

--- C# DB/Entity Framework Core/Exam - 18.04.2019/MusicHub/Data/Configuration.cs
+++ C# DB/Entity Framework Core/Exam - 18.04.2019/MusicHub/Data/Configuration.cs
@@ -2,7 +2,7 @@
 {
    public static class Configuration
     {
-        public static string ConnectionString = 
-            @"Server=.;Database=MusicHub;Trusted_Connection=True";
+        public static string ConnectionString =
+            @"Server=DESKTOP-2A8QSKB\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
     }
 }
--- C# DB/Entity Framework Core/Exam - 18.04.2019/MusicHub/Data/MusicHubDbContext.cs
+++ C# DB/Entity Framework Core/Exam - 18.04.2019/MusicHub/Data/MusicHubDbContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace MusicHub.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
 
     public class MusicHubDbContext : DbContext
     {
@@ -13,6 +14,15 @@
         {
         }
 
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
@@ -24,7 +34,22 @@
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder);
+            builder
+                .Entity<SongPerformer>(entity =>
+                {
+                    entity.HasKey(sp => new { sp.PerformerId, sp.SongId });
+
+                    entity
+                        .HasOne(p => p.Performer)
+                        .WithMany(s => s.PerformerSongs)
+                        .HasForeignKey(p => p.PerformerId);
+
+                    entity
+                        .HasOne(s => s.Song)
+                        .WithMany(p => p.SongPerformers)
+                        .HasForeignKey(s => s.SongId);
+
+                });
         }
     }
 }
