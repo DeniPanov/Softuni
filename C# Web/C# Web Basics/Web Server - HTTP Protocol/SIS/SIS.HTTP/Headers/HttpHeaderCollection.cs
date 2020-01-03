c7a715420a47fefc2206550b839eeab3ab3a3e1e
DeniPanov <Inedvonap@gmail.com>
2020-01-03 07:45:10 +0200

    Added HttpHeader.cs and HttpHeaderCollection.cs

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/SIS.HTTP/Headers/HttpHeaderCollection.cs
@@ -0,0 +1,49 @@
+﻿namespace SIS.HTTP.Headers
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using SIS.HTTP.Common;
+    using SIS.HTTP.Headers.Contracts;
+
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private Dictionary<string, HttpHeader> httpHeaders;
+
+        public HttpHeaderCollection()
+        {
+            this.httpHeaders = new Dictionary<string, HttpHeader>();
+        }
+
+        public void AddHeader(HttpHeader header)
+        {
+            CoreValidator.ThrowIfNull(header, nameof(header));
+            this.httpHeaders.Add(header.Key, header);
+        }
+
+        public bool ContainsHeader(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+            return this.httpHeaders.ContainsKey(key);           
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            if (this.httpHeaders.ContainsKey(key) == false) //consider commenting this check
+            {
+                return null;
+            }
+
+            return this.httpHeaders[key];
+        }
+
+        public override string ToString()
+        {
+            return string.Join(GlobalConstants.HttpNewLine,
+                this.httpHeaders.Values.
+                Select(header => header.ToString()));
+        }
+    }
+}
