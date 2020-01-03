ba1291be4f52e162a8dc1f4d57e724c97c6dad9a
DeniPanov <Inedvonap@gmail.com>
2020-01-03 06:52:51 +0200

    Added StringExtensions.cs

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/SIS.HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SIS.HTTP.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string text)
+        {
+            return char.ToUpper(text[0]) + text.Substring(1);
+        }
+    }
+}
