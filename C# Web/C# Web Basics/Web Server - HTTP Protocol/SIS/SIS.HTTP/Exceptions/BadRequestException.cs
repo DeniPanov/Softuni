ec7acd071dd51a0551a329ef1fd89741ce7772c3
DeniPanov <Inedvonap@gmail.com>
2020-01-03 06:52:26 +0200

    Added BadRequestException.cs and InternalServerErrorException.cs

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/SIS.HTTP/Exceptions/BadRequestException.cs
@@ -0,0 +1,20 @@
+﻿namespace SIS.HTTP.Exceptions
+{
+    using System;
+
+    public class BadRequestException : Exception
+    {
+        private const string BadRequestExecptionDefaultMessage =
+            "The Request was malformed or contains unsupported elements.";
+
+        public BadRequestException() 
+            : this(BadRequestExecptionDefaultMessage)
+        {
+        }
+
+        public BadRequestException(string name)
+            : base(name)
+        {
+        }
+    }
+}
---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/SIS.HTTP/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,20 @@
+﻿namespace SIS.HTTP.Exceptions
+{
+    using System;
+
+    public class InternalServerErrorException : Exception
+    {
+        private const string InternalServerExecptionDefaultMessage =
+            "The Server has encountered an error.";
+
+        public InternalServerErrorException()
+            : this(InternalServerExecptionDefaultMessage)
+        {
+        }
+
+        public InternalServerErrorException(string name)
+            : base()
+        {
+        }
+    }
+}
