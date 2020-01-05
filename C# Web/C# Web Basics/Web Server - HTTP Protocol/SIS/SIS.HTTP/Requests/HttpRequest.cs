aaea1fe59490ad610d8a990464ef9b1c69916d7e
DeniPanov <Inedvonap@gmail.com>
2020-01-05 10:27:00 +0200

    Added HttpRequest.cs

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/SIS.HTTP/Requests/HttpRequest.cs
@@ -0,0 +1,36 @@
+﻿namespace SIS.HTTP.Requests
+{
+    using System.Collections.Generic;
+
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Common;
+    using SIS.HTTP.Headers;
+    using SIS.HTTP.Headers.Contracts;
+    using SIS.HTTP.Requests.Contracts;
+
+    public class HttpRequest : IHttpRequest
+    {
+        public HttpRequest(string requestString)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(requestString, nameof(requestString));
+
+            this.FormData = new Dictionary<string, object>();
+            this.QueryData = new Dictionary<string, object>();
+            this.Headers = new HttpHeaderCollection();
+
+            //TODO: parse request data...
+        }
+
+        public string Path { get; private set; }
+
+        public string Url { get; private set; }
+
+        public Dictionary<string, object> FormData { get; }
+
+        public Dictionary<string, object> QueryData { get; }
+
+        public IHttpHeaderCollection Headers { get; }
+
+        public HttpRequestMethod RequestMethod { get; private set; }
+    }
+}
