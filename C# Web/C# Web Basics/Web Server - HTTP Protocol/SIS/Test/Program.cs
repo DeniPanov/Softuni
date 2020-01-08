0cdfc03f7f7ccedfa78ab4f2d9454fe427d6921d
DeniPanov <Inedvonap@gmail.com>
2020-01-08 07:21:09 +0200

    Added Program.cs for testing

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Test
+{
+    using System;
+
+    using SIS.HTTP.Requests;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string request = 
+                "POST /someUrl/asd?name=pesho&id=1#fragment HTTP/1.1\r\n"
+                + "Authorization: Basic 23442562\r\n"
+                + "Date: " + DateTime.UtcNow + "\r\n"
+                + "Host: localhost:5000\r\n"
+                + "\r\n"
+                +"username=peshoGoshov&password=abv";
+
+            HttpRequest httpRequest = new HttpRequest(request);
+
+            ;
+        }
+    }
+}
