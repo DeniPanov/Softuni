cccd9760314d8704e9ec14156ef993620441a884
DeniPanov <Inedvonap@gmail.com>
2020-01-10 21:54:39 +0200

    Added all needed result classes

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/SIS.WebServer/Results/HtmlResult.cs
@@ -0,0 +1,18 @@
+﻿namespace SIS.WebServer.Results
+{
+    using System.Text;
+
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Headers;
+    using SIS.HTTP.Responses;
+
+    public class HtmlResult : HttpResponse
+    {
+        public HtmlResult(string content, HttpResponseStatusCode responseStatusCode)
+            : base(responseStatusCode)
+        {
+            this.Headers.AddHeader(new HttpHeader("ContentType", "text/html; charset=utf-8"));
+            this.Content = Encoding.UTF8.GetBytes(content);
+        }
+    }
+}
---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/SIS.WebServer/Results/TextResult.cs
@@ -0,0 +1,27 @@
+﻿namespace SIS.WebServer.Results
+{
+    using System.Text;
+
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Headers;
+    using SIS.HTTP.Responses;
+
+    public class TextResult : HttpResponse
+    {
+        public TextResult(string content, HttpResponseStatusCode responseStatusCode,
+            string contentType = "text/plain; charset=utf-8")
+            : base(responseStatusCode)
+        {
+            this.Headers.AddHeader(new HttpHeader("ContentType", contentType));
+            this.Content = Encoding.UTF8.GetBytes(content);
+        }
+
+        public TextResult(byte[] content, HttpResponseStatusCode responseStatusCode,
+            string contentType = "text/plain; charset=utf-8")
+            : base(responseStatusCode)
+        {
+            this.Headers.AddHeader(new HttpHeader("ContentType", contentType));
+            this.Content = content;
+        }
+    }
+}
