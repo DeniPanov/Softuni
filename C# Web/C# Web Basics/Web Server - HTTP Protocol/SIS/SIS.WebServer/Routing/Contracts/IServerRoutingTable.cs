851e821a46ecf6e3593f8f40a3fb8151ba2a1a5b
DeniPanov <Inedvonap@gmail.com>
2020-01-11 09:51:50 +0200

    Added IServerRoutingTable.cs

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/SIS.WebServer/Routing/Contracts/IServerRoutingTable.cs
@@ -0,0 +1,17 @@
+﻿namespace SIS.WebServer.Routing.Contracts
+{
+    using System;
+
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Requests.Contracts;
+    using SIS.HTTP.Responses.Contracts;
+
+    public interface IServerRoutingTable
+    {
+        void Add(HttpRequestMethod method, string path, Func<IHttpRequest, IHttpResponse> func);
+
+        bool Contains(HttpRequestMethod requestMethod, string path);
+
+        Func<IHttpRequest, IHttpResponse> Get(HttpRequestMethod requestMethod, string path);
+    }
+}
