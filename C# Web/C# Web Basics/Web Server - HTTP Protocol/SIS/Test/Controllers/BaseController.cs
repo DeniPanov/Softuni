371440cebc98fd7090561d466420fce8c54d3b03
DeniPanov <Inedvonap@gmail.com>
2020-01-13 07:44:49 +0200

    Added BaseController.cs and HomeController.cs

---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/Test/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿namespace Test.Controllers
+{
+    using System.IO;
+    using System.Runtime.CompilerServices;
+
+    using SIS.HTTP.Enums;
+    using SIS.WebServer.Results;
+    using SIS.HTTP.Responses.Contracts;
+
+    public abstract class BaseController
+    {
+        public IHttpResponse View([CallerMemberName] string view = null)
+        {
+            string controllerName = this.GetType().Name.Replace("Controller", string.Empty);
+            string viewName = view;
+
+            string viewContent = File.ReadAllText("Views/" + controllerName + "/" + viewName + ".html");
+
+            return new HtmlResult(viewContent, HttpResponseStatusCode.Ok);
+        }
+    }
+}
---
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/Test/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace Test.Controllers
+{
+    using SIS.HTTP.Requests.Contracts;
+    using SIS.HTTP.Responses.Contracts;
+
+    public class HomeController : BaseController
+    {
+        public IHttpResponse Home(IHttpRequest request)
+        {
+            return this.View();
+        }
+    }
+}
