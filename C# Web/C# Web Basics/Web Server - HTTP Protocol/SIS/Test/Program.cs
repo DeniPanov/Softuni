2a619f80e9c31a1b9e7fac915d5e19c8d5ef6b63
DeniPanov <Inedvonap@gmail.com>
2020-01-13 07:44:18 +0200

    Modified Program.cs so the whole app runs :-)

--- C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/Test/Program.cs
+++ C# Web/C# Web Basics/Web Server - HTTP Protocol/SIS/Test/Program.cs
@@ -1,13 +1,20 @@
 ﻿namespace Test
 {
     using System;
-    using System.Globalization;
     using System.Net;
     using System.Text;
+    using System.Globalization;
+
+    using SIS.WebServer;
     using SIS.HTTP.Enums;
     using SIS.HTTP.Headers;
+    using Test.Controllers;
     using SIS.HTTP.Requests;
     using SIS.HTTP.Responses;
+    using SIS.WebServer.Results;
+    using SIS.WebServer.Routing;
+    using SIS.HTTP.Responses.Contracts;
+    using SIS.WebServer.Routing.Contracts;
 
     public class Program
     {
@@ -23,15 +30,22 @@
 
             //HttpRequest httpRequest = new HttpRequest(request);
 
-            HttpResponseStatusCode statusCode = HttpResponseStatusCode.Created;
+            //HttpResponseStatusCode statusCode = HttpResponseStatusCode.Created;
 
-            HttpResponse response = new HttpResponse(statusCode);
-            response.AddHeader(new HttpHeader("Host", "localhost:5000"));
-            response.AddHeader(new HttpHeader("Date", DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)));
+            //HttpResponse response = new HttpResponse(statusCode);
+            //response.AddHeader(new HttpHeader("Host", "localhost:5000"));
+            //response.AddHeader(new HttpHeader("Date", DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)));
 
-            response.Content = Encoding.UTF8.GetBytes("<h1>Hello world!</h1>");
+            //response.Content = Encoding.UTF8.GetBytes("<h1>Hello world!</h1>");
 
-            Console.WriteLine(Encoding.UTF8.GetString(response.GetBytes()));
+            //Console.WriteLine(Encoding.UTF8.GetString(response.GetBytes()));
+
+            IServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+            serverRoutingTable.Add(HttpRequestMethod.Get, "/", httpRequest
+                => new HomeController().Home(httpRequest));
+
+            Server server = new Server(32000, serverRoutingTable);
+            server.Run();
         }
     }
 }
