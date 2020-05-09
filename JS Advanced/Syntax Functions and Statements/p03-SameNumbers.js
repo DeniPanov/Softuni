8c3dce51529b66718000c82b2cdb1783bf574faf
DeniPanov <Inedvonap@gmail.com>
2020-05-09 23:37:57 +0300

    Add p03-SameNumbers.js

---
+++ JS Advanced/Syntax Functions and Statements/p03-SameNumbers.js
@@ -0,0 +1,17 @@
+function solve(x){
+    x = x.toString()
+        .split("");
+
+    let firstNumber = x[0];
+    hasSameNumbers = x.some(x => x !== firstNumber);
+
+    let sum = x
+        .map(Number)
+        .reduce((a, c) => (a + c));
+
+    console.log(!hasSameNumbers);
+    console.log(sum);
+}
+
+solve(2222222);
+solve(1234);
