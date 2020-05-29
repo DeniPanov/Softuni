b84cd6d626035f25a81b4b35cad607b8bbe6877e
DeniPanov <Inedvonap@gmail.com>
2020-05-29 06:21:54 +0300

    Refactor p07-MagicMatrix.js

--- JS Advanced/Arrays/p07-MagicMatrix.js
+++ JS Advanced/Arrays/p07-MagicMatrix.js
@@ -1,8 +1,8 @@
 "use strict"
 
-function solve(matrix){
+function solve(matrix) {
 
-    function getRowSum(row){
+    function getRowSum(row) {
         return row.reduce((a, c) => a + c);
     }
 
@@ -11,23 +11,23 @@ function solve(matrix){
     let isMagicMatrix = true;
 
     for (let i = 0; i < matrix.length; i++) {
-        if(getRowSum(matrix[i]) !== firstRowSum || getRowSum(reverseMatrix[i]) !== firstRowSum){
+        if (getRowSum(matrix[i]) !== firstRowSum || getRowSum(reverseMatrix[i]) !== firstRowSum) {
             isMagicMatrix = false;
             break;
         }
     }
 
-    console.log(isMagicMatrix);
+    return isMagicMatrix;
 }
 
-solve([[4, 5, 6],
-       [6, 5, 4],
-       [5, 5, 5]]);
+console.log(solve([[4, 5, 6],
+[6, 5, 4],
+[5, 5, 5]]));
 
-   solve([[11, 32, 45],
-         [21, 0, 1],
-         [21, 1, 1]]);
+console.log(solve([[11, 32, 45],
+[21, 0, 1],
+[21, 1, 1]]));
 
-   solve([[1, 0, 0],
-         [0, 0, 1],
-         [0, 1, 0]]);
+console.log(solve([[1, 0, 0],
+[0, 0, 1],
+[0, 1, 0]]));
