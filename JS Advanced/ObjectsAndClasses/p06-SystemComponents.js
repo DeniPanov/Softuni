3c8ebf9c8e294a62094ea6946a6d6b2411a245b9
DeniPanov <Inedvonap@gmail.com>
2020-06-03 07:20:42 +0300

    Complete p06-SystemComponents.js

--- JS Advanced/ObjectsAndClasses/p06-SystemComponents.js
+++ JS Advanced/ObjectsAndClasses/p06-SystemComponents.js
@@ -1,15 +1,10 @@
 "use strict"
 
 function solve(input) {
-    function sortByTwoCriteria(arr){
-            return arr.sort((a, b) => a.length - b.length ||
-                a.localeCompare(b));
-        }
-
     let db = {};
-    
+
     for (const item of input) {
-        let[system, component, sub] = item.split(" | ");
+        let [system, component, sub] = item.split(" | ");
 
         if (db.hasOwnProperty(system) === false) {
             db[system] = {};
@@ -22,19 +17,31 @@ function solve(input) {
         db[system][component].push(sub);
     }
 
-    console.log(db);
+    Object.entries(db).sort((a, b) => {
+        return Object.keys(b[1]).length - Object.keys(a[1]).length ||
+            a[0].localeCompare(b[0]);
+    }).forEach(([system, component]) => {
+        console.log(system);
+        Object.entries(component).sort((a, b) => b[1].length - a[1].length)
+            .forEach(([name, sub]) => {
+                console.log("|||" + name);
+                sub.forEach(s => {
+                    console.log("||||||" + s);
+                })
+            })
+    })
 }
 
 console.log(solve(['SULS | Main Site | Home Page',
-'SULS | Main Site | Login Page',
-'SULS | Main Site | Register Page',
-'SULS | Judge Site | Login Page',
-'SULS | Judge Site | Submittion Page',
-'Lambda | CoreA | A23',
-'SULS | Digital Site | Login Page',
-'Lambda | CoreB | B24',
-'Lambda | CoreA | A24',
-'Lambda | CoreA | A25',
-'Lambda | CoreC | C4',
-'Indice | Session | Default Storage',
-'Indice | Session | Default Security']));
+    'SULS | Main Site | Login Page',
+    'SULS | Main Site | Register Page',
+    'SULS | Judge Site | Login Page',
+    'SULS | Judge Site | Submittion Page',
+    'Lambda | CoreA | A23',
+    'SULS | Digital Site | Login Page',
+    'Lambda | CoreB | B24',
+    'Lambda | CoreA | A24',
+    'Lambda | CoreA | A25',
+    'Lambda | CoreC | C4',
+    'Indice | Session | Default Storage',
+    'Indice | Session | Default Security']));
