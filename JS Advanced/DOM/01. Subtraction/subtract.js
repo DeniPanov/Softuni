bb1b2819e67bddc16bc41c824e5b8aba7c5684e6
DeniPanov <Inedvonap@gmail.com>
2020-06-06 15:56:53 +0300

    Add subtract.js

--- JS Advanced/DOM/01. Subtraction/subtract.js
+++ JS Advanced/DOM/01. Subtraction/subtract.js
@@ -1,3 +1,5 @@
 function subtract() {
-    console.log('TODO:...');
+    let num1 = Number(document.querySelector("#firstNumber").value);
+    let num2 = Number(document.getElementById("secondNumber").value);
+    document.getElementById("result").textContent = num1 - num2;
 }
