4ae948bee6cdd705cbf9aa831c3851fa2ec53a5f
DeniPanov <Inedvonap@gmail.com>
2020-06-10 20:37:37 +0300

    Add p02 Fill-Dropdown and p03 Accordion

--- JS Advanced/DOM/02. Fill-Dropdown/dropdown.js
+++ JS Advanced/DOM/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.querySelector("#newItemText");
+    let value = document.querySelector("#newItemValue");
+    const option = document.createElement("option");
+
+    option.textContent = text.value;
+    option.value = value.value;
+    document.querySelector("#menu").appendChild(option);
+
+    text.value = "";
+    value.value = "";
 }
--- JS Advanced/DOM/03. Accordion/accordion.js
+++ JS Advanced/DOM/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let text = document.querySelector("#extra");
+    let btn = document.querySelectorAll(".button")[0];
+    
+    if (text.style.display === "none") {
+        text.style.display = "block";
+        btn.textContent = "Less";
+    } else {
+        text.style.display = "none"
+        btn.textContent = "More";
+    }
 }
