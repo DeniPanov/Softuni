35820723ff4f6f482f4dbb29f42b849c46c459c4
DeniPanov <Inedvonap@gmail.com>
2020-07-10 23:55:55 +0300

    Add p01-BusStop and p02-BusSchedule

--- JS Applications/REST Services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/02.BUS-SCHEDULE/app.js
+++ JS Applications/REST Services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,42 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    let next = `depot`;
+    let name = ``;
+    const input = {
+        info() { return document.getElementById('info') },
+        depart() { return document.getElementById('depart') },
+        arrive() { return document.getElementById('arrive') }
     }
 
+
+    function depart() {
+        let URL = `https://judgetests.firebaseio.com/schedule/${next}.json`;
+        fetch(URL)
+            .then((x) => x.json())
+            .then((result) => showInfo(result));
+
+        function showInfo(data) {
+            input.info().textContent = `Next stop ${data.name}`;
+            name = data.name;
+            next = data.next;
+            disabled();
+        }
+    }
     function arrive() {
-        console.log('Arrive TODO...');
+        input.info().textContent = `Arriving at ${name}`;
+        disabled();
+    }
+
+    function disabled() {
+        const but = input.arrive().disabled;
+        if (but) {
+            input.arrive().disabled = false;
+            input.depart().disabled = true;
+        }
+        else {
+            input.arrive().disabled = true;
+            input.depart().disabled = false;
+        }
     }
 
     return {
